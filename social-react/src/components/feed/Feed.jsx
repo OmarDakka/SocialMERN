6c423e028bb1a4d76a6144f3fad0629294dc5dbf
OmarDakka <omardakka42@gmail.com>
2022-01-22 02:57:52 +0200

    finished top bar, and left bar as UI

---
+++ social-react/src/components/feed/Feed.jsx
@@ -0,0 +1,12 @@
+import Share from "../share/Share";
+import "./feed.css"
+
+export default function Feed() {
+  return (
+  <div className="feed">
+      <div className="feedWraper">
+        <Share/>
+      </div>
+  </div>
+  );
+}
