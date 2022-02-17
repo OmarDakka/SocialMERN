48e5d59aa0f9e2d2e6288883d602350ac5ada3e3
OmarDakka <omardakka42@gmail.com>
2022-02-17 09:13:54 +0200

    like/dislike function works with axios as intended

--- social-react/src/context/AuthContext.js
+++ social-react/src/context/AuthContext.js
@@ -2,9 +2,19 @@ import { createContext, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-  user: null,
-  isFetching: false,
-  error: false,
+user : {
+  _id : "61e876b9b4daf76e80379d02",
+  username : "jane",
+  email : "jane@gmail.com",
+  password : "123456",
+  profilePicture : "",
+  coverPicture : "",
+  followers : [],
+  following : [],
+  isAdmin : false
+  },
+  isFetching : false,
+  error: false
 };
 
 export const AuthContext = createContext(INITIAL_STATE);
