f19ba76b2699677699d43d6c7a7bdbbe0cc12b4e
OmarDakka <omardakka42@gmail.com>
2022-02-19 13:07:46 +0200

    added http errors library to index to create http errors and handle them

--- NODE-REST-API/index.js
+++ NODE-REST-API/index.js
@@ -9,13 +9,16 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const path = require("path");
+const createHttpError = require("http-errors");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL,
-     {useNewUrlParser: true, useUnifiedTopology: true},()=>{
-    console.log("Connected to MongoDB")
-    }
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
 );
 
 app.use("/images", express.static(path.join(__dirname, "public/images")));
@@ -26,26 +29,41 @@ app.use(helmet());
 app.use(morgan("common"));
 
 const storage = multer.diskStorage({
-    destination:(req,file,cb) => {
-        cb(null,"public/images");
-    },
-    filename: (req,file,cb) => {
-        cb(null, req.body.name)
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-const upload = multer({storage: storage});
-app.post("/api/upload", upload.single("file"), (req,res) => {
-    try {
-        return res.status(200).json("file uploaded successfully.")
-    } catch (error) {
-        console.log(error);
-    }
-})
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("file uploaded successfully.");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
-app.listen(8800, ()=>{
-    console.log("Backend server is running");
-})
+
+app.use((req, res, next) => {
+    next(new createHttpError.NotFound("Endpoint not found"));
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+
+  if (process.env.NODE_ENV === "production") {
+    delete err.stack;
+  }
+
+  return res.status(err.status || 500).json(err);
+});
+
+app.listen(8800, () => {
+  console.log("Backend server is running");
+});
